7885aabdc84dd6298912d6261ede9953a9e63c6d
ist187646 <daniela.alexandram@gmail.com>
2021-05-21 18:12:29 +0100

    tracking 99% done

---
+++ detection.m
@@ -0,0 +1,60 @@
+close all, clear all
+
+path = 'View_001/';
+
+nFrames = size(dir([path '/*.jpg']), 1); %dir da-nos uma lista com os nomes de todas as frames .jpg e o size da-nos o numero de frames
+
+% as proximas duas linhas foram calculadas aqui para conseguirmos criar o Bkg com o tamanho das frames
+strFrame = sprintf('%s%s%.4d.%s', path, 'frame_', 0, 'jpg'); %string com o nome da primeira frame
+I = imread(strFrame);
+Bkg = zeros(size(I)); %imagem a zeros (inicialmente) do tamanho das nossas frames
+
+alfa = 0.005;
+
+% for i=0:(nFrames-1) %vamos percorrer todas as frames
+%     strFrame = sprintf('%s%s%.4d.%s', path, 'frame_', i, 'jpg');
+%     Y = imread(strFrame);
+%     Bkg = alfa * double(Y) + (1-alfa) * double(Bkg);
+%     imagesc(uint8(Bkg)); axis ij, drawnow
+% end
+% 
+% imwrite(uint8(Bkg), 'bkg.png');
+
+imgbk = imread('bkg.png');
+
+thr = 75;
+minArea = 200;
+
+se = strel('disk', 3);
+
+figure;
+for i=0:(nFrames-1) % ler frames sequencialmente e para cada imagem calcular a diferença com a imagem de background 
+    strFrame = sprintf('%s%s%.4d.%s', path, 'frame_', i, 'jpg');
+    imgfr = imread(strFrame); %para ir buscar cada imagem
+    imshow(imgfr); hold on;
+    
+    imgdif = (abs(double(imgbk(:,:,1))-double(imgfr(:,:,1))) > thr) | (abs(double(imgbk(:,:,2))-double(imgfr(:,:,2))) > thr) | (abs(double(imgbk(:,:,3))-double(imgfr(:,:,3))) > thr);
+    % imgdif só fica ativo (a 1) no sítio das onde há movimento aka onde há
+    % diferenças
+
+    bw = imclose(imgdif, se); %aplicar operação morfológica para limpara as regiões um bocadinho melhor
+    % mesmo assim acusa muitas regiões ativas que são ruído - regionprops:
+    
+    [lb num] = bwlabel(bw);
+    regionProps = regionprops(lb, 'Area', 'BoundingBox', 'FilledImage', 'Centroid');
+    inds = find([regionProps.Area] > minArea); % guarda os indices das regiões que satisfazem a condição
+    regnum = length(inds); % numero de regiões que nos interessam
+    
+    if regnum
+        for j=1:regnum
+            [lin col] = find(lb == inds(j));
+            upLPoint = min([lin col]);
+            dWindow = max([lin col]) - upLPoint + 1;
+            
+            rectangle('Position', [fliplr(upLPoint) fliplr(dWindow)], 'EdgeColor', [1 1 0], 'linewidth', 2);
+        end
+    end
+            
+    drawnow    
+    
+end
